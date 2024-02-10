60279f6292edaad3e666ada9155d3eaba7ecdc82
alamnahid <nahidalam13765@gmail.com>
2024-02-10 21:01:38 +0600

    variable code

---
+++ Variables and Data Types/Variable.js
@@ -0,0 +1,19 @@
+// using var keyword 
+var a = 10;
+var b = 20;
+var sum = a+b;
+console.log('sum ', sum);
+
+// using let keyword
+let c = 20;
+let d = 30;
+let summation = a+b;
+console.log('sum ', sum)
+
+// using const keyword
+
+const num1 = 10;
+const num2 = 40;
+
+const multiply = num1 * num2;
+console.log(multiply)
