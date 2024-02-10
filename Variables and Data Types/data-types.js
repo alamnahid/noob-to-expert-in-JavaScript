52f96560980c9b58c880e11d66eef13501c4eba3
alamnahid <nahidalam13765@gmail.com>
2024-02-10 21:02:57 +0600

    data types code

---
+++ Variables and Data Types/data-types.js
@@ -0,0 +1,46 @@
+// numbers types data
+let number1 = 10;
+let number2 = 20;
+console.log('number type ', number1, number2);
+
+// char type
+let char = 'a';
+console.log('character type ', char);
+
+// String type
+let fname = 'Nahid';
+let lname = "Alam";
+
+console.log('String type ', fname+lname);
+
+// booleann type 
+let x = false;
+let y = true;
+console.log('boolean type ', x,y)
+
+// object 
+const person = {
+    personName: 'Nahid Alam',
+    age: 22
+}
+console.log('object type ', person);
+
+// array
+const cars = ['volvo', 'audi', 'bmw'];
+console.log('array types ', cars)
+
+// date object 
+const date = new Date("2022-.3-25")
+
+// javascript evaluates expressons from left to right
+let t = 16+4+"Nahid";
+let z = "Nahid" +16+4;
+console.log(t)
+console.log(z)
+
+const flaoting = 34.45;
+console.log(flaoting)
+
+//Exponential Notation
+let ay = 123e5; 
+console.log(ay)
