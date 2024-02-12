88ad7ec2a13ded19510deec05fe7c6be9530e093
alamnahid <nahidalam13765@gmail.com>
2024-02-12 22:15:00 +0600

    string search

---
+++ Strings/string-search.js
@@ -0,0 +1,9 @@
+// JavaScript String indexOf()
+// The indexOf() method returns the index (position) of the first occurrence of a string in a string, or it returns -1 if the string is not found
+
+const text1 = "I am Nahid Alam Nahid";
+
+console.log(text1.indexOf('Nahid'));
+console.log(text1.indexOf('is'));
+
+console.log(text1.lastIndexOf('Nahid'));
