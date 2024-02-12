09e54ffb4b8306bf876d9c3302d45203bef30dad
alamnahid <nahidalam13765@gmail.com>
2024-02-12 21:11:36 +0600

    javascript string

---
+++ Strings/string.js
@@ -0,0 +1,26 @@
+// You can use single or double quotes to write a string 
+const name = 'Nahid Alam';
+const name2 = "Nahid Alam";
+console.log(name)
+console.log(name2)
+
+// Quotes Inside Quotes
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+console.log(answer1)
+console.log(answer2)
+console.log(answer3)
+
+
+// Template Strings
+
+const myname = `Nahid Alam`;
+console.log(myname)
+
+console.log(myname.length); //check the string length
+
+// Escape Characters
+const text = "my name is \"Nahid\" Alam";
+console.log(text)
