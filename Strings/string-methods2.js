9bac40e9f6c160abab9abc5e58d8e47fab98ea6e
alamnahid <nahidalam13765@gmail.com>
2024-02-12 21:42:20 +0600

    string methods part 2

---
+++ Strings/string-methods2.js
@@ -0,0 +1,18 @@
+// JavaScript String padStart()
+
+let text = "5";
+let padded = text.padStart(6,"0"); // output 000005
+let padded2 = text.padEnd(4,"0"); // output 5000
+
+console.log(padded)
+console.log(padded2)
+
+let text2 = "Hello world!";
+let result = text2.repeat(4); // its repeat full text 4 times
+console.log(result)
+
+
+let text3 = "Please visit Microsoft!";
+let newText = text.replace("Microsoft", "W3Schools");
+
+console.log(newText)
