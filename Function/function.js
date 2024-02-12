c37148519cd2a68231f3273199d7b6b8b601e7ae
alamnahid <nahidalam13765@gmail.com>
2024-02-12 21:08:49 +0600

    javascript function

---
+++ Function/function.js
@@ -0,0 +1,23 @@
+function addition (a,b){
+    return a+b;
+}
+
+console.log(addition(5,5))
+
+function multliplication (a,b){
+    return a*b;
+}
+console.log(multliplication(5,5))
+
+function division (a,b) {
+    console.log(a/b);
+}
+
+division(5,5)
+
+// arrow function in javascript 
+const modulus = (a,b)=>{
+    return a%b
+}
+
+console.log(modulus(10,3))
