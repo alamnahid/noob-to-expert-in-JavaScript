81d58102a2b2985e81e46d1895c22d3331886d9b
alamnahid <nahidalam13765@gmail.com>
2024-02-13 22:31:32 +0600

    number methods in javascript

---
+++ Number Methods/number-methods.js
@@ -0,0 +1,20 @@
+const x = 123;
+console.log(typeof(x))
+console.log(x.toString())
+console.log(x.toExponential(8))
+
+
+const number = 2.656565;
+console.log(number.toFixed(2))
+
+console.log(number.toPrecision(4))
+
+// parseInt
+const num1 = "233";
+console.log(num1)
+console.log(parseInt(num1))
+
+// parse float
+const num2 = "2333";
+console.log(num2);
+console.log(parseFloat(num2))
